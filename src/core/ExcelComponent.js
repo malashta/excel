395ce9996535870c7de2f3b037a13cdc6142408c
Tallerr <malashta@gmail.com>
2020-05-25 09:42:05 +0700

    add listeners

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,10 +1,18 @@
 import {DomListener} from '@/core/DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
+
   /*
     возвращает шаблон компонента
    */
   toHTML() {
     return '';
+  }
+
+  init() {
+    this.initDOMListeners();
   }
 }
