b589dfd360f0ccfc69682c3ee6b0b83899cee5fa
Tallerr <malashta@gmail.com>
2020-06-09 11:36:10 +0700

    add mousedown resize method

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -20,10 +20,14 @@ export class Table extends ExcelComponent {
       const $resizer = $(event.target);
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
+
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
+
       document.onmousemove = e => {
         const delta = e.pageX - coords.right;
         const value = coords.width + delta;
         $parent.$el.style.width = value + 'px';
+        cells.forEach(el => el.style.width = value + 'px');
       };
 
       document.onmouseup = () => {
